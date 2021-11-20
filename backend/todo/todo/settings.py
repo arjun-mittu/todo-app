76af5ece20eb76e32065e03130c08c2ea4e6a45c
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2021-11-20 21:09:51 +0530

    completed frontend

--- backend/todo/todo/settings.py
+++ backend/todo/todo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+     "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'todo.urls'
@@ -125,3 +128,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    
+    "http://localhost:3000",
+]
